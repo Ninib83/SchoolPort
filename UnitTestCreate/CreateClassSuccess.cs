466a37ebaabc18030908e1c3c8c20959acb67589
kevin larsson <larsson__96@hotmail.com>
2016-05-02 10:40:35 +0200

    new test added

---
+++ UnitTestCreate/CreateClassSuccess.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SchoolPort;
+using System.Collections.Generic;
+
+namespace UnitTestCreate
+{
+    [TestClass]
+    public class CreateClassSuccess
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            School school = new School();
+            //arrange
+            List<string[]> list = new List<string[]>();
+            list.Add(new string[] { "Class" });
+            string input = "1,Th3,John";
+            bool sum = true;
+
+            //act
+            var result = school.Create(list, input);
+
+            //assert
+            Assert.AreEqual(sum, result);
+        }
+    }
+}
---
+++ UnitTestCreate/CreateCourseSuccess.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SchoolPort;
+using System.Collections.Generic;
+
+namespace UnitTestCreate
+{
+    [TestClass]
+    public class CreateCourseSuccess
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            School school = new School();
+            //arrange
+            List<string[]> list = new List<string[]>();
+            list.Add(new string[] { "Course" });
+            string input = "1,Th3,John,th2,2016-05-02,2016-05-05";
+            bool sum = true;
+
+            //act
+            var result = school.Create(list, input);
+
+            //assert
+            Assert.AreEqual(sum, result);
+        }
+    }
+}
